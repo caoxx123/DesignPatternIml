96ab651107276c72a0d09941affe6996f0a4a2e0
caoxx123 <caotao0923@gmail.com>
2019-05-18 11:45:33 +0800

    add adapterPattern iml

---
+++ src/io/caoxx123/o6AdapterPattern/service/AdvancedMediaPlayer.java
@@ -0,0 +1,7 @@
+package io.caoxx123.o6AdapterPattern.service;
+
+
+public interface AdvancedMediaPlayer {
+    public void playVlc(String fileName);
+    public void playMp4(String fileName);
+}
---
+++ src/io/caoxx123/o6AdapterPattern/service/serviceiml/AudioPlayer.java
@@ -0,0 +1,18 @@
+package io.caoxx123.o6AdapterPattern.service.serviceiml;
+
+import io.caoxx123.o6AdapterPattern.service.MediaPlayer;
+
+public class AudioPlayer implements MediaPlayer {
+    MediaAdapeter mediaAdapeter;
+    @Override
+    public void play(String adudioType, String fileName) {
+        if(adudioType.equalsIgnoreCase("MP3")){
+            System.out.println(fileName+adudioType);
+        }else if(adudioType.equalsIgnoreCase("VLC")||adudioType.equalsIgnoreCase("MP4")){
+            mediaAdapeter=new MediaAdapeter(adudioType);
+            mediaAdapeter.play(adudioType,fileName);
+        }else {
+            System.out.println("the "+adudioType +"not supported");
+        }
+    }
+}
